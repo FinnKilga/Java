65b3781261e39be3213b21d5828b31c4e6080465
FinnKilga <finn.kilga@student.htldornbirn.at>
2022-09-27 16:23:27 +0200

    TicTacToe begonnen

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,52 +7,21 @@ public class TicTacToe
         Scanner scanner = new Scanner(System.in);
         String input;
         char[][] gamefield = new char[2][2];
-        char player1 = 'X';
-        char player2 = 'O';
-        int playerSelection = 0;
-        System.out.println("Mit diesem Programm kann man zu zweit Tic Tac Toe spielen");
-        System.out.println("Spieler 1: X");
-        System.out.println("Spieler 2: O");
-        System.out.println("Gib immer Zeile und Spalte an!");
-        int row = 0;
-        int collumn = 0;
-        boolean finish = false;
-        boolean wrongEntry;
-        while (finish)
+        int row;
+        int col;
+        boolean finish = false
+
+        System.out.println("Mit diesem Programm kann man TicTacToe spielen");
+
+        while (!finish)
         {
-            do
-            {
-                System.out.println("Bitte gib die Zeile an");
-                input = scanner.nextLine();
-                row = input.charAt(0);
-                System.out.println("Bitte gib die Spalte an");
-                input = scanner.nextLine();
-                collumn = input.charAt(0);
-                if (gamefield[row][collumn] != null)
-                {
-
-                }
-            }
-            while (wrongEntry);
-
-            if (playerSelection == 0)
-            {
-                gamefield[row][collumn] = 'X';
-                playerSelection = 1;
-            }
-            else
-            {
-                gamefield[row][collumn] = 'O';
-                playerSelection = 0;
-            }
-            finish = F_finish(gamefield);
+            System.out.println("Bitte gib die Zeile an");
+            input = scanner.nextLine();
+            row = input.charAt(0);
+            System.out.println("Bitte gib die Spalte an");
+            input = scanner.nextLine();
+            col = input.charAt(0);
         }
     }
-    public static boolean F_finish(char[][] gamefield = new char[2][2])
-    {
-        if (gamefield[0][0] == 'X' && gamefield[0][1] == 'X' && gamefield[0][2]) == 'X')
-        {
-            return true;
-        }
-    }
+
 }
